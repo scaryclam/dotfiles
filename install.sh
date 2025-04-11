4dc5f3c54660b581f075e849ea0ca52711ab2c16
Becky Lewis <becky.lewis@legl.com>
2025-04-11 10:08:26 +0100

    Updated to fix issues

--- install.sh
+++ install.sh
@@ -3,7 +3,4 @@
 echo "Install QoL packages"
 apt update -y && apt install -y openssh-client curl wget tar build-essential
 
-echo "Install ssh as not all containers have it installed"
-apt install openssh-client
-
 echo "Made things sane"
